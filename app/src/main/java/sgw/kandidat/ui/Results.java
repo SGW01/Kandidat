a722a09ba1584ccf08319bcfad568d6eb0bcd005
Kate <kate.severinko.work@gmail.com>
2018-04-19 09:52:58 +0300

    new design in

--- app/src/main/java/sgw/kandidat/ui/Results.java
+++ app/src/main/java/sgw/kandidat/ui/Results.java
@@ -3,38 +3,31 @@ package sgw.kandidat.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import sgw.kandidat.R;
-import sgw.kandidat.dagger.base.BaseActivity;
-import sgw.kandidat.dagger.components.ActivityComponent;
-import sgw.kandidat.dagger.modules.DataBaseModule;
-import sgw.kandidat.dagger.modules.SharedPreferenceModule;
+import sgw.kandidat.database.DataBaseModule;
 
 /**
  * Created by Катя on 25.12.2017.
  */
 
-public class Results extends BaseActivity implements View.OnClickListener {
+public class Results extends AppCompatActivity implements View.OnClickListener {
 
-    @Inject
-    DataBaseModule dataBaseModule;
 
-    @Named(SharedPreferenceModule.AUTH_PREF)
-    @Inject
+    DataBaseModule dataBaseModule = new DataBaseModule();
+
     SharedPreferences sharedPreferencesName;
 
     TextView tv_recomend, tv_type_kandidat;
     Button btn_back;
     String name;
-    int [] data = new int[8];
-    int resultInBal=0;
+    int[] data = new int[8];
+    int resultInBal = 0;
     String type;
     String allResult;
 
@@ -44,129 +37,99 @@ public class Results extends BaseActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_layout);
-        tv_recomend=(TextView)findViewById(R.id.tv_recomend_zp);
-        tv_type_kandidat=(TextView)findViewById(R.id.tv_type_kandidat);
-        btn_back=(Button)findViewById(R.id.btn_back);
+        tv_recomend = (TextView) findViewById(R.id.tv_recomend_zp);
+        tv_type_kandidat = (TextView) findViewById(R.id.tv_type_kandidat);
+        btn_back = (Button) findViewById(R.id.btn_back);
         btn_back.setOnClickListener(this);
-        name = sharedPreferencesName.getString("auth"," ");
+        name = sharedPreferencesName.getString("auth", " ");
         Log.d(TAG, name);
-        data = dataBaseModule.readFromDB(this,name);
+        data = dataBaseModule.readFromDB(this, name);
         countResult();
         allResult = "Кандидат " + name + " " + type + "!";
         tv_type_kandidat.setText(allResult);
-        String string = "Рекомендована заробітня плата становить: " + String.valueOf(resultInBal*200);
+        String string = "Рекомендована заробітня плата становить: " + String.valueOf(resultInBal * 200);
         tv_recomend.setText(string);
     }
 
-    private void countResult(){
+    private void countResult() {
         //age
-        if (data[0]<25)
-        {
+        if (data[0] < 25) {
             resultInBal = 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-        if (data[0]>=25&&data[0]<50)
-        {
-            resultInBal=5;
+        } else if (data[0] >= 25 && data[0] < 50) {
+            resultInBal = 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //stazh
-        if (data[1]>5)
-        {
-            resultInBal+=10;
+        if (data[1] > 5) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-            if (data[1]>=2&&data[1]<=5)
-        {
-            resultInBal+=5;
+        } else if (data[1] >= 2 && data[1] <= 5) {
+            resultInBal += 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //vo
-        if (data[2]>1)
-        {
-            resultInBal+=10;
+        if (data[2] > 1) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-        if (data[2]==1)
-        {
-            resultInBal+=5;
+        } else if (data[2] == 1) {
+            resultInBal += 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //vp
-        if (data[3]>1)
-        {
-            resultInBal+=10;
+        if (data[3] > 1) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-        if (data[3]==1)
-        {
-            resultInBal+=5;
+        } else if (data[3] == 1) {
+            resultInBal += 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //dz
-        if (data[4]>1)
-        {
-            resultInBal+=10;
+        if (data[4] > 1) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-        if (data[4]==1)
-        {
-            resultInBal+=5;
+        } else if (data[4] == 1) {
+            resultInBal += 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //dk
-        if (data[5]==1)
-        {
-            resultInBal+=10;
+        if (data[5] == 1) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //dv
-        if (data[6]==1)
-        {
-            resultInBal+=10;
+        if (data[6] == 1) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
         }
         //inm
-        if (data[7]>2)
-        {
-            resultInBal+=10;
+        if (data[7] > 2) {
+            resultInBal += 10;
             Log.d(TAG, String.valueOf(resultInBal));
-        }
-        else
-        if (data[7]>0&&data[7]<=2)
-        {
-            resultInBal+=5;
+        } else if (data[7] > 0 && data[7] <= 2) {
+            resultInBal += 5;
             Log.d(TAG, String.valueOf(resultInBal));
         }
 
-        if (resultInBal>50)
-            type="Найкращий кандидат";
+        if (resultInBal > 50)
+            type = "Найкращий кандидат";
+        else if (resultInBal <= 50 && resultInBal > 25)
+            type = "Альтернативний кандидат";
         else
-            if (resultInBal<=50&&resultInBal>25)
-                type = "Альтернативний кандидат";
-        else
-            type="Не відповідний";
+            type = "Не відповідний";
         Log.d(TAG, String.valueOf(resultInBal));
 
-    }
-    @Override
-    public void inject(ActivityComponent injector) {
-        injector.inject(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_back:
-                Intent intent = new Intent(this,MainActivity.class);
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
-            default:break;
+            default:
+                break;
         }
     }
 }
